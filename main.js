c3fec513e0e32be85e1f8990ccf2d2735ed14a4f
MitaliShah <mitalishaleenshah@gmail.com>
2024-04-09 18:02:59 -0500

    mobile tooltip

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-import "./style.css";
+// import "./style.css";
+
+// export default function toggle() {
+//   let tooltip = document.getElementById("share");
+//   if (tooltip.style.display === "flex") {
+//     tooltip.style.display = "none";
+//   } else {
+//     tooltip.style.display = "flex";
+//   }
+// }
